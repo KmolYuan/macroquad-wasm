f17e650644082001477609375a4e94d6ca494422
KmolYuan <pyslvs@gmail.com>
2022-05-14 20:58:17 +0800

    Add edge option.

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,20 @@ use macroquad::{main, prelude::*};
 #[main("Macroquad Example")]
 async fn main() {
     let mut name = "World".to_string();
+    let mut sides = 8;
     loop {
         clear_background(WHITE);
         let [x, y] = [screen_width() * 0.5, screen_height() * 0.5];
-        draw_poly(x, y, 8, 200., 0., GRAY);
+        draw_poly(x, y, sides, 200., 90., GRAY);
         egui_macroquad::ui(|ctx| {
             egui::Window::new("Main Window").show(ctx, |ui| {
                 ui.heading(&format!("Hello {name}!"));
                 ui.label("What's your name?");
                 ui.text_edit_singleline(&mut name);
+                let drag = egui::DragValue::new(&mut sides)
+                    .prefix("Edges: ")
+                    .clamp_range(3..=20);
+                ui.add(drag);
             });
         });
         egui_macroquad::draw();
