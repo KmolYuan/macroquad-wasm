b54f19569bc8e2bcf060eaafb1b078fd1438372c
KmolYuan <pyslvs@gmail.com>
2022-05-14 20:26:56 +0800

    Draw a background.

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,8 @@ async fn main() {
     let mut name = "World".to_string();
     loop {
         clear_background(WHITE);
+        let [x, y] = [screen_width() * 0.5, screen_height() * 0.5];
+        draw_poly(x, y, 8, 200., 0., GRAY);
         egui_macroquad::ui(|ctx| {
             egui::Window::new("Main Window").show(ctx, |ui| {
                 ui.heading(&format!("Hello {name}!"));
